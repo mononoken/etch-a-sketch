43c6c624e449ecdc049ce46a0d01f57356a9efbf
mononoken <ken.maeshima@pm.me>
2021-10-21 07:22:41 -0500

    Working event listener that adds class to fill square on mouseover.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const grid = document.getElementById('the-grid');
 
+const gridSquares = [];
+
 function createGrid(squareQuantity) {
   let newDiv;
   for (let i=0; i < squareQuantity; i++) {
     newDiv = document.createElement('div');
     newDiv.className = 'grid-square';
+    gridSquares.push(newDiv);
     grid.appendChild(newDiv);
   }
 }
 
 createGrid(256);
+
+for (let gridSquare=0; gridSquare < gridSquares.length; gridSquare++) {
+  gridSquares[gridSquare].addEventListener("mouseover", () => {
+    gridSquares[gridSquare].classList.add("grid-square-filled");
+  }, false);
+}
